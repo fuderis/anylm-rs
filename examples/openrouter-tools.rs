822c8b3eb78bc5f73f8114756bf2c8c70cc98e94
Bulat Sh. (fuderis) <synapdrake@ya.ru>
2026-06-27 01:21:01 +0500

    update examples

--- examples/openrouter-tools.rs
+++ examples/openrouter-tools.rs
@@ -1,53 +1,67 @@
-use anylm::{AiChunk, Completions, Schema, Tool, prelude::*};
+use anylm::{AiChunk, Completions, Messages, Proxy, Schema, Tool, ToolCall};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
 
+/// The weather tool data
+#[allow(dead_code)]
+#[derive(Debug, serde::Deserialize)]
+struct LocationData {
+    location: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    // read api key:
     let api_key = std::env::var("OPENROUTER_API_KEY")?;
 
-    /// The weather tool data
-    #[allow(dead_code)]
-    #[derive(Debug, serde::Deserialize)]
-    struct LocationData {
-        location: String,
-    }
+    // create messages:
+    let messages = Messages::new()
+        .system(vec!["You are a helpful assistant.".into()])
+        .user(vec!["What's the weather like in London?".into()])
+        .wrap();
 
     // send request:
     let mut response = Completions::openrouter(api_key, "qwen/qwen3-vl-30b-a3b-thinking")
         .proxy(Proxy::all("socks5://127.0.0.1:1080")?)
-        .user_message(vec!["What's the weather like in London?".into()])
         .tool(
             Tool::new("weather", "Search weather by location")
                 .required_property("location", Schema::string("The location")),
         )
-        .send()
+        .send(messages.clone())
         .await?;
 
-    // read response stream:
     let mut tool_calls = vec![];
+
+    // read response chunks:
     while let Some(chunk) = response.next().await {
         match chunk? {
-            AiChunk::Text { text } => {
-                eprint!("{text}");
+            AiChunk::Text(text_part) => {
+                eprint!("{text_part}");
             }
-            AiChunk::Tool { id, name, json_str } => {
-                tool_calls.push((id, name, json_str));
+            AiChunk::Tool(tool_call) => {
+                tool_calls.push(tool_call);
             }
         }
     }
-    println!();
 
     // handle tool calls:
-    for (id, name, json_str) in tool_calls {
-        match name.as_ref() {
+    for ToolCall { id, func, .. } in tool_calls {
+        match func.name.as_ref() {
             "weather" => {
-                let location: LocationData = serde_json::from_str(&json_str)?;
-                println!("Tool call [{id}]: {location:#?}");
+                let results = get_weather(func.parse_args()?);
+                println!("{results}");
+
+                messages.lock().await.add_tool(id, vec![results.into()])
             }
             _ => {}
         }
     }
 
+    // dbg!(&messages);
+
     Ok(())
+}
+
+fn get_weather(_loc: LocationData) -> String {
+    format!("74°F, Cloudy\n• Precipitation: 0%\n• Humidity: 64%\n• Wind: 3.11 mph")
 }
